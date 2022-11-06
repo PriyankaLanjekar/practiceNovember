fce16066046e4b19860527f14260f4951b859f50
DELL <DELL@DESKTOP-O60VPK0>
2022-11-06 12:39:53 +0530

    updared

--- src/Class1.java
+++ src/Class1.java
@@ -2,7 +2,7 @@
 public class Class1 {
 	public static void main(String[] args) {
 		System.out.println("Hare Krishna");
-		System.out.println("hare ram");
+		System.out.println("hare ramhare ramhare ramhare ramhare ram");
 	}
 
 }
